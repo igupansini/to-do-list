4f971721d4e1b72a89dd7c38b682a74b9445368e
Igor Pansini <pansini@outlook.com.br>
2025-01-26 19:26:06 -0300

    Gateway tests

--- gateway/src/main/java/com/impansini/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/impansini/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.impansini.todo;
+package com.impansini.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class TodoApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
--- todo/src/main/java/com/impansini/todo/rest/TaskResource.java
+++ todo/src/main/java/com/impansini/todo/rest/TaskResource.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/todo/api")
 public class TaskResource {
 
     private final Logger log = LoggerFactory.getLogger(TaskResource.class);
