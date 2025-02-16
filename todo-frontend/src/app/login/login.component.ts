054ce92d228a2274530030f8ced077210d21d04c
Igor Pansini <pansini@outlook.com.br>
2025-02-16 16:03:17 -0300

    creating login component in angular

---
+++ todo-frontend/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const {username, password} = this.loginForm.value;
+      console.log('Username:', username);
+      console.log('Password:', password);
+    }
+  }
+
+}
