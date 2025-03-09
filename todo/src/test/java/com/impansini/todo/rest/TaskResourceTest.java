e416e20e83108f59eb3acc430941f443a870286a
Igor Pansini <pansini@outlook.com.br>
2025-03-09 09:43:30 -0300

    junit TaskResourceTest 1

---
+++ todo/src/test/java/com/impansini/todo/rest/TaskResourceTest.java
@@ -0,0 +1,26 @@
+package com.impansini.todo.rest;
+
+import org.junit.jupiter.api.Test;
+
+class TaskResourceTest {
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void findOneTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
